fd2f517b078fb7d7c6446e66e37dda4dd1d145e2
orasmaem <144736197+orasmaem@users.noreply.github.com>
2024-03-30 18:22:24 +0200

    Add files via upload
    tegin veel tehingute jaoks eraldi logifaili, siis on vb parem silma peal hoida mis toimub

---
+++ Kliendid.java
@@ -0,0 +1,114 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class Kliendid implements Runnable {
+    // Failide teekondade ja muude konstantide määratlemine
+    private String inventar = "inventaar.txt"; // Inventarifail
+    private String logiFail = "tehingud.txt"; // Tehingute logifail
+    private String[] tooted = {"Piim", "Sai", "Juust", "Sink"}; // Olemasolevad tooted
+    private int maksimum = 3; // Maksimaalne kogus, mida iga klient saab osta iga toote kohta
+    private Random suvaline = new Random(); // Juhusliku arvu generaator
+    private SimpleDateFormat kuupäevaVormindaja = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); // Kuupäeva-vormindaja
+    private int klientideLoendur = 1; // Klientide loendur, alustab esimesest kliendist
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                int klientideArv = suvaline.nextInt(10) + 1; // Juhuslik klientide arv (1 kuni 10)
+                for (int i = 0; i < klientideArv; i++) {
+                    uuendaInventari();
+                }
+                Thread.sleep(10000); // Ootamine enne järgmist tsüklit
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Meetod inventari uuendamiseks vastavalt klientide ostudele
+    private void uuendaInventari() throws IOException {
+        // Lae inventarifailist praegune inventar
+        Map<String, Integer> inventariKaart = loeInventar();
+
+        // Ava logifail kirjutamiseks
+        FileWriter logiKirjutaja = new FileWriter(logiFail, true);
+        PrintWriter logiPrintija = new PrintWriter(logiKirjutaja);
+        logiPrintija.println(kuupäevaVormindaja.format(new Date()) + " Toimusid tehingud: ");
+        // Käi läbi iga toode
+        for (String toode : tooted) {
+            // Juhuslik kogus, mida iga klient ostab (0 kuni maksimum)
+            int ostetudKogus = suvaline.nextInt(maksimum + 1);
+
+            // Kirjuta tehingu info logifaili
+            logiPrintija.println(klientideLoendur + ". Klient ostis " + ostetudKogus + " " + toode);
+
+            // Võta praegune kogus inventarifailist
+            int praeguneKogus = inventariKaart.getOrDefault(toode, 0);
+            // Arvuta uus kogus, võttes arvesse klientide ostusid
+            int uusKogus = Math.max(praeguneKogus - ostetudKogus, 0);
+            // Uuenda kaarti uue kogusega
+            inventariKaart.put(toode, uusKogus);
+        }
+
+        // Sulge logifail
+        logiPrintija.close();
+
+        // Ava inventarifail lisamiseks
+        FileWriter kirjuta = new FileWriter(inventar, true);
+        PrintWriter prindiFaili = new PrintWriter(kirjuta);
+
+        // Kirjuta uuendatud inventar inventarifaili
+        for (Map.Entry<String, Integer> sissekanne : inventariKaart.entrySet()) {
+            prindiFaili.println(sissekanne.getKey() + ", " + sissekanne.getValue());
+        }
+
+        // Sulge fail
+        prindiFaili.close();
+
+        // Suurenda klientide loendurit järgmise ostu jaoks
+        klientideLoendur++;
+    }
+
+    // Meetod inventarifailist inventari lugemiseks ja kaardi tagastamiseks
+    private Map<String, Integer> loeInventar() throws IOException {
+        Map<String, Integer> inventariKaart = new HashMap<>();
+
+        // Ava inventarifail lugemiseks
+        BufferedReader lugeja = new BufferedReader(new FileReader(inventar));
+        String rida;
+        while ((rida = lugeja.readLine()) != null) {
+            String[] osad = rida.split(", ");
+            if (osad.length == 2) {
+                String toode = osad[0];
+                int kogus = Integer.parseInt(osad[1]);
+                inventariKaart.put(toode, kogus);
+            }
+        }
+        // Sulge fail
+        lugeja.close();
+
+        // Tagasta kaart
+        return inventariKaart;
+    }
+
+    // Meetod praeguse kuupäeva ja kellaaja vormindamiseks
+    private String kuupäevaVormindus() {
+        return kuupäevaVormindaja.format(new Date());
+    }
+
+    // Main meetod
+    public static void main(String[] args) {
+        Kliendid kliendid = new Kliendid();
+        Thread thread = new Thread(kliendid);
+        thread.start();
+    }
+}
--- Laadung.java
+++ Laadung.java
@@ -1,45 +1,66 @@
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
 
-public class Laadung implements Runnable {//peab runnable kasutama, et saaks threadi samal ajal jooksutada
-
-    private String inventar = "inventaar.txt";//fail kuhu kirjutame
-    private String[] tooted = {"Piim", "Sai", "Juust", "Sink"};//erinevad tooted, saab muuta
-    private int[] kogused = new int[tooted.length];//iga toote kogus
-    private int maksimaalne = 10;//maksimaalne toodete lisamine
-    private Random random = new Random();
+public class Laadung implements Runnable {
+    private String inventar = "inventaar.txt"; // Inventari faili tee
+    private String logiFail = "tehingud.txt"; // Logi faili tee
+    private String[] tooted = {"Piim", "Sai", "Juust", "Sink"}; // Saadaval olevad tooted
+    private int[] kogused = new int[tooted.length]; // Toodete kogused laos
+    private int maksimaalne = 10; // Maksimaalne kogus, mida saab igat toodet lisada
+    private Random suvaline = new Random(); // Juhusliku arvu generaator
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); // Kuupäeva-vormindaja
 
     @Override
-    public void run() {//runnable classist voetud
+    public void run() {
         try {
-            //alustan suvaliste arvudega 1-10(maksimaalne)
-            for (int i = 0; i < tooted.length; i++) {
-                kogused[i] = random.nextInt(maksimaalne) + 1;
-            }
-            // numbrite uuendamise loogika
             while (true) {
-                FileWriter kirjuta = new FileWriter(inventar, false); //append mode false, kirjutab ule mitte ei lisa: https://stackoverflow.com/questions/1225146/java-filewriter-with-append-mode
-                PrintWriter prindiFaili = new PrintWriter(kirjuta);//prindib faili
-                for (int i = 0; i < tooted.length; i++) {//kaib iga toote labi, kirjutab toote nime toodetest[] ja siis uuendab numbrit
-                    kogused[i] += random.nextInt(maksimaalne) + 1;
-                    prindiFaili.println(tooted[i] + ", " + kogused[i]);
-                    System.out.println(tooted[i] + ", " + kogused[i]);
-                }
-                prindiFaili.close();//salvestus
-                Thread.sleep(10000);//paus
+                uuendaInventari(); // Uuendage inventari
+                Thread.sleep(10000); // Oodake enne järgmist uuendust
             }
-
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
-        }//errori puudmine
+        }
     }
 
-    public static void main(String[] args) {//kasutades runnable interface saame mitu threadi(lõime) korraga kasutada
+    // Meetod inventari uuendamiseks
+    private void uuendaInventari() throws IOException {
+        FileWriter kirjuta = new FileWriter(inventar, false); // Ava inventarifail kirjutamiseks
+        PrintWriter prindiFaili = new PrintWriter(kirjuta);
+
+        FileWriter logiKirjutaja = new FileWriter(logiFail, true); // Ava logifail kirjutamiseks
+        PrintWriter logiPrintija = new PrintWriter(logiKirjutaja);
+
+        // Kirjutage uute toodete kogused logifaili
+        logiPrintija.println(getTimestamp() + " Uued tooted saabusid:");
+        for (int i = 0; i < tooted.length; i++) {
+            int uusKogus = suvaline.nextInt(maksimaalne) + 1;
+            kogused[i] += uusKogus;
+            logiPrintija.println(uusKogus + " " + tooted[i]);
+        }
+
+        // Kirjutage uuendatud inventar inventarifaili
+        for (int i = 0; i < tooted.length; i++) {
+            prindiFaili.println(tooted[i] + ", " + kogused[i]);
+            System.out.println(tooted[i] + ", " + kogused[i]);
+        }
+
+        prindiFaili.close(); // Sulgege inventarifail
+        logiPrintija.close(); // Sulgege logifail
+    }
+
+    // Meetod praeguse kuupäeva ja kellaaja vormindamiseks
+    private String getTimestamp() {
+        return dateFormat.format(new Date());
+    }
+
+    // Main meetod
+    public static void main(String[] args) {
         Laadung laadung = new Laadung();
-        Thread thread = new Thread(laadung);//wrapib n-ö ära ja siis alustab tööd samaaegselt teise meetodiga
+        Thread thread = new Thread(laadung);
         thread.start();
-        //https://www.simplilearn.com/tutorials/java-tutorial/thread-in-java
     }
 }
