c2f86e4d7418bdf4f151ce555bed4ccfc27ed332
philip120 <109464319+philip120@users.noreply.github.com>
2024-03-30 21:13:48 +0200

    Update Laadung.java

--- Laadung.java
+++ Laadung.java
@@ -48,13 +48,4 @@ public class Laadung implements Runnable {//peab runnable kasutama, et saaks thr
             e.printStackTrace();
         }//errori puudmine
     }
-
-    public static void main(String[] args) {//kasutades runnable interface saame mitu threadi(lõime) korraga kasutada
-        Laadung laadung = new Laadung();
-        Thread thread = new Thread(laadung);//wrapib n-ö ära ja siis alustab tööd samaaegselt teise meetodiga
-        Kliendid kliendid=new Kliendid();
-        Thread thread2 =new Thread(kliendid);
-        thread.start();
-        thread2.start();
-    }
 }
